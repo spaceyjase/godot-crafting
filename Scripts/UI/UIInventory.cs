b999880fc4b5645ddd0f3fd0efb3d6ea280cbd04
Jason <429978+spaceyjase@users.noreply.github.com>
2021-07-02 14:05:23 +0100

    Instance the selected item in scene.

--- Scripts/UI/UIInventory.cs
+++ Scripts/UI/UIInventory.cs
@@ -7,12 +7,17 @@ using Godot.Collections;
 
 public class UIInventory : Node2D
 {
+  [Export] private readonly PackedScene selectedItemScene;
+  
   private readonly List<SlotPanel> panels = new List<SlotPanel>();
+  private Position2D selectedItem;
   
   public override void _Ready()
   {
     base._Ready();
     panels.AddRange(this.GetChildren<SlotPanel>());
+    selectedItem = selectedItemScene.Instance<Position2D>();
+    AddChild(selectedItem);
   }
 
   public void AddItem(Item item)
